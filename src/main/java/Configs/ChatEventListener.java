aa61ce6e584a46d29e2816d729f69177e0f093ed
FiJg <filip.jng@gmail.com>
2023-01-19 10:12:51 +0100

    chateventlistener websocket

--- src/main/java/Configs/ChatEventListener.java
+++ src/main/java/Configs/ChatEventListener.java
@@ -4,24 +4,36 @@ import Models.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 //webs
 public class ChatEventListener {
 
+    private static final Logger logger = LoggerFactory.getLogger(ChatEventListener.class);
+
     @Autowired
     private SimpMessageSendingOperations messagingTemplate;
+    /*
+    public ChatEventListener(SimpMessagingTemplate messagingTemplate, SimpMessageSendingOperations sendingOperations) {
+        this.messagingTemplate = messagingTemplate;
+    }
+    */
 
     @EventListener
     public void handleWSConnectListener(SessionConnectedEvent event){
+        logger.info("new socket connection");
         System.out.println("Have a new web socket conn");
     }
 
     @EventListener
     public void handleWSDisconnectListener(SessionDisconnectEvent event){
-
+        logger.info("user disconnected");
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
 
         String username = (String) headerAccessor.getSessionAttributes().get("username");
@@ -33,7 +45,7 @@ public class ChatEventListener {
 
             chatMessage.setSender(username);
 
-            messagingTemplate.convertAndSend(chatMessage);
+            messagingTemplate.convertAndSend("/topic", chatMessage);
         }
 
     }
---
+++ src/main/java/Configs/WSConfig.java
@@ -0,0 +1,44 @@
+package Configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WSConfig implements WebSocketMessageBrokerConfigurer {
+
+
+    @Value("${spring.rabbitmq.host}")
+    String host;
+
+    @Value("${spring.rabbitmq.username}")
+    String username;
+
+    @Value("${spring.rabbitmq.password}")
+    String password;
+
+    public static final String TOPIC_DESTINATION_PREFIX = "/topic/";
+    public static final String REGISTRY = "/ws";
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint(REGISTRY)
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableStompBrokerRelay(TOPIC_DESTINATION_PREFIX)
+                .setRelayHost(host)
+                .setClientLogin(username)
+                .setClientPasscode(password)
+                .setSystemLogin(username)
+                .setSystemPasscode(password);
+    }
+
+
+}
